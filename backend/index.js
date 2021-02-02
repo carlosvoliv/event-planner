b6376f3c00dab5e42e493f9d2f3eb2d50d8f2c35
Carlos Eduardo Vaz <carlos.voliveira@al.infnet.edu.br>
2021-02-02 12:31:00 -0300

    updates feitos na hora do código

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-console.log('alguma outra coisa')
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+})
+
+app.get('/formulario', (req, res, next) => {
+    res.render('formulario');
+})
+
+app.get('/sobre', (req, res, next) => {
+    res.render('sobre');
+})
+
+app.listen(3001, () => {
+    console.log('O servidor está rodando')
+})
