2ef16d2f90810719c730aa70a59cf40f8e4e2db3
Carlos Eduardo Vaz <carlos.voliveira@al.infnet.edu.br>
2021-02-04 10:49:23 -0300

    body-parser, middleware e tranformando em pdf

--- backend/controllers/evento-controller.js
+++ backend/controllers/evento-controller.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const ejs = require('ejs')
+const htmlToPdf = require('html-pdf-node')
 
 const handlerGetEvento = (req, res, next) => {
     console.log('evento ok')
@@ -5,9 +8,45 @@ const handlerGetEvento = (req, res, next) => {
 }
 
 const handlerPostEvento = (req, res, next) => {
-    res.send({
-        mensagem:"teste do POST Evento"
+    // console.log(req)
+    // console.log(req.body)
+
+    const body = req.body
+
+    // TO-DO: criar view model para render evento
+
+    const viewModel = {
+        nome: body.name,
+        email: body.email,
+        age: body.age,
+    }
+
+    // TO-DO: criar template
+
+    var htmlText = fs.readFileSync('./views/evento-pdf.ejs', 'utf8')
+    console.log(htmlText)
+
+    
+    // TO-DO: criar html
+
+    var htmlRenderized = ejs.render(htmlText, viewModel)
+    console.log(htmlRenderized)
+
+    // TO-DO: transformar em pdf
+
+    let file = { content: htmlRenderized }
+    let options = { format: 'A4' }
+
+    htmlToPdf.generatePdf(file, options)
+    .then(pdfBuffer => {
+            res.contentType("application/pdf")
+            res.send(pdfBuffer)
     })
+
+
+    // res.send({
+    //     mensagem:"teste do POST Evento"
+    // })
 }
 
 module.exports = {
--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,13 @@ const express = require('express')
 const path = require('path')
 const eventoController = require('./controllers/evento-controller')
 const sobreController = require('./controllers/sobre-controller')
+const bodyParser = require('body-parser')
 const PORT = 3001
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
