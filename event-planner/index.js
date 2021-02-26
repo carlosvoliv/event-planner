4e387f495eb20a1ef752d65eb9e50a68c3dbbd26
Carlos Eduardo Vaz <carlos.voliveira@al.infnet.edu.br>
2021-02-26 09:29:03 -0300

    alterando porta e perfumaria

--- event-planner/index.js
+++ event-planner/index.js
@@ -1,33 +1,31 @@
-console.log(':: servidor funcionando, manda bala')
+console.log(":: servidor funcionando, manda bala");
 
-const express = require('express')
-const path = require('path')
-const eventoController = require('./controllers/evento-controller')
-const sobreController = require('./controllers/sobre-controller')
-const bodyParser = require('body-parser')
-const PORT = 3001
+const express = require("express");
+const path = require("path");
+const eventoController = require("./controllers/evento-controller");
+const sobreController = require("./controllers/sobre-controller");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3001;
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')))
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 const handlerRoot = (req, res, next) => {
-  console.log(':: index rodando suave')
-  res.render('index')
-}
+  console.log(":: index rodando suave");
+  res.render("index");
+};
 
-app.get('/', handlerRoot)
-app.get('/evento', eventoController.get)
-app.post('/evento', eventoController.post)
+app.get("/", handlerRoot);
+app.get("/evento", eventoController.get);
+app.post("/evento", eventoController.post);
 
+app.get("/sobre", sobreController.get);
 
-app.get('/sobre', sobreController.get)
-
-
-app.listen(PORT, () => {
-  console.log(':: se liga, vc esta conectado na porta :' + PORT)
-})
+app.listen(port, () => {
+  console.log(":: se liga, vc esta conectado na porta :" + port);
+});
--- event-planner/models/sexo-model.js
+++ event-planner/models/sexo-model.js
@@ -1,32 +1,32 @@
 const data = [
   {
-    id: '1',
-    descricao: 'Masculino'
+    id: "1",
+    descricao: "Masculino",
   },
   {
-    id: '2',
-    descricao: 'Feminino'
-  }
-]
+    id: "2",
+    descricao: "Feminino",
+  },
+];
 
 const getById = (id) => {
-  console.log('Início getById: ' + id)
+  console.log("Início getById: " + id);
 
   const resposta = data.find((item) => {
-    return item.id === id
-  })
+    return item.id === id;
+  });
 
-  console.log('resposta método geById: ')
-  console.log(resposta)
+  console.log("resposta método geById: ");
+  console.log(resposta);
 
-  return resposta
-}
+  return resposta;
+};
 
 const getAll = () => {
-  return data
-}
+  return data;
+};
 
 module.exports = {
   getAllSexo: getAll,
-  getSexoPorId: getById
-}
+  getSexoPorId: getById,
+};
